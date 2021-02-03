f27ddf7de21b81657020e60bae74855506232e65
Akash Verma <akashv1507@gmai.com>
2021-02-03 14:12:53 +0530

    minor changes

--- src/revisionwiseErrorStorage/revisionwiseErrorCreator.py
+++ src/revisionwiseErrorStorage/revisionwiseErrorCreator.py
@@ -27,8 +27,8 @@ def createRevisionwiseError(startDate: dt.datetime, endDate: dt.datetime, config
 
     currDate = startDate
     insertSuccessCount = 0
-    # revisionList = ['R0A', 'R0B', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16']
-    revisionList = ['R0A']
+    revisionList = ['R0A', 'R0B', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16']
+    # revisionList = ['R0A']
 
     while currDate <= endDate:
         for revisionNo in revisionList:
